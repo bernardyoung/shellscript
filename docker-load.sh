08cb2c065f9e36e854754ae07ae7aca4056e824b
hdyqbb <315857423@qq.com>
2018-07-02 15:06:42 +0800

    Delete docker-load.sh

--- docker-load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for images_tar_gz in $(ls | grep tar.gz);
-        do
-        tar -zxvf $images_tar_gz
-        images_tar=${images_tar_gz%.gz}
-        docker load < $images_tar
-        rm -rf $images_tar_gz
-done
