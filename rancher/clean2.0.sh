93a2219bcfe19a7417a651795b915da8beb37175
Bernard Young <315857423@qq.com>
2018-12-27 09:52:11 +0800

    Create clean2.0.sh

---
+++ rancher/clean2.0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+df -h|grep kubelet |awk -F % '{print $2}'|xargs umount
+rm /var/lib/kubelet/* -rf
+rm /etc/kubernetes/* -rf
+rm /var/lib/rancher/* -rf
+rm /var/lib/etcd/* -rf
+rm /var/lib/cni/* -rf
+iptables -F && iptables -t nat –F
+ip link del flannel.1
+docker ps -a|awk '{print $1}'|xargs docker rm -f
+docker volume ls|awk '{print $2}'|xargs docker volume rm
